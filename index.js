14c4a7156d453db8b8a9de137142798a1eb962a3
Andrew Wooldridge <triptych+c9@gmail.com>
2018-04-15 06:46:49 +0000

    adding index.js

--- index.js
+++ index.js
@@ -5,4 +5,17 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "welcome to enchanted sticker book"});
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(" -- server is listening on port "+ process.env.PORT);
+})
